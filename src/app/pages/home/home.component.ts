507618479745aaac92c9aad31c4be0c8eacfbc35
zrtch <1160639597@qq.com>
2025-05-12 22:54:22 +0800

    feat: 表单

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, MatCardModule, MatButtonModule],
+  imports: [
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
