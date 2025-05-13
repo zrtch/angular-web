6d841918ff0dafcc8994427f333f952300e9764b
zrtch <1160639597@qq.com>
2025-05-13 12:08:36 +0800

    feat: icon图标

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -11,10 +16,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     RouterLink,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
     MatSlideToggleModule,
     MatCheckboxModule,
+    MatButtonToggleModule,
+    MatChipsModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {}
